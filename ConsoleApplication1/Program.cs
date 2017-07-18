5e39348b1cab3341b25894b1b39e7e838f16eee4
propeersinfoKSM <ksm.propeersinfo@gmail.com>
2017-07-18 16:57:13 +0530

    for test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		public void test()
+		{
+
+
+			FileStream fs = new FileStream("D:\\Sample.txt", FileMode.Open, FileAccess.Read);
+			//StreamReader sr=new StreamReader(fs);
+			BinaryReader br = new BinaryReader(fs);
+			byte[] info = br.ReadBytes((int)fs.Length);
+																																							 
+			br.Close();
+			fs.Close();
+
+			FileStream fs2 = new FileStream("D:\\Test.txt", FileMode.Create, FileAccess.Write);
+			BinaryWriter bw = new BinaryWriter(fs2);
+			bw.Write(info);
+			bw.Close();
+			fs2.Close();
+
+		}
+		static void Main(string[] args)
+		{
+
+			Program p = new Program();
+		}
+
+
+	}
+}
